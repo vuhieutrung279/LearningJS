cd80aae84fd04ae67df17d50cea1b304b14b1e09
Trung Vu <vuhieutrung279@gmail.com>
2020-05-07 20:59:11 +0700

    enhanced object literals

--- array-like.js
+++ array-like.js
@@ -16,3 +16,15 @@ function arrFrom() {
     return numbers.map(x => x*x);
 }
 console.log(arrFrom(5,4,3,2,1));
+
+
+// enhanced object literals
+const name = 'Trung';
+const body = {
+    name,
+    run() {
+        console.log(`${this.name} is running`);
+    }
+}
+
+body.run();
